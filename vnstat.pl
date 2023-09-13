008b351676b8c5a2bc8186c3d3418018c1921364
monaco <arianitkukaj@gmail.com>
2023-09-13 19:16:44 +0200

    Update vnstat.pl

--- vnstat.pl
+++ vnstat.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 use AnyEvent::HTTPD;
- 
+
 my $httpd = AnyEvent::HTTPD->new (port => 9090);
 my $hostname = qx(hostname);
 
 sub gather {
-	my $summary = qx(vnstat -i eth0);
+	my $summary = qx(vnstat -i enp41s0);
 	my $monthly = qx(/usr/bin/vnstat -m);
 	my $daily = qx(/usr/bin/vnstat -d);
 	my $hourly = qx(/usr/bin/vnstat -h);
@@ -14,17 +14,77 @@ sub gather {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>vnstat - $hostname</title>
+    <meta charset="utf-8">
+    <title>vnstat - $hostname</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f0f0f0;
+            color: #333;
+        }
+
+        header {
+            background-color: #0078d4;
+            color: white;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+            margin: 20px 0;
+        }
+
+        hr {
+            border: 1px solid #ccc;
+        }
+
+        pre {
+            background-color: #f8f8f8;
+            padding: 10px;
+            border-radius: 5px;
+            overflow: auto;
+        }
+
+        code {
+            font-family: Consolas, Monaco, monospace;
+            background-color: #f1f1f1;
+            padding: 2px 6px;
+            border-radius: 3px;
+            color: #333;
+        }
+
+        p {
+            text-align: center;
+            font-style: italic;
+            color: #777;
+        }
+    </style>
 </head>
 <body>
-	<h1>Host: $hostname</h1><hr>
-	<h1>Summary:</h1><pre><code>$summary</code></pre><hr>
-	<h1>Monthly:</h1><pre><code>$monthly</code></pre><hr>
-	<h1>Daily:</h1><pre><code>$daily</code></pre><hr>
-	<h1>Hourly:</h1><pre><code>$hourly</code></pre>
+    <header>
+        <h1>vnstat - $hostname</h1>
+    </header>
+    <main>
+	<pre>
+<p><img alt="" src="https://x.4solution.no/vnstat.png" style="height:370px; width:500px" /></p>
+<p><img alt="" src="https://x.4solution.no/summary.png" style="height:200px; width:500px" /><br />
+<br />
+<img alt="" src="https://x.4solution.no/network-log.png" style="height:134px; width:500px" /><br />
+<br />
+<img alt="" src="https://x.4soltuion.no/hour.png" /><img alt="" src="https://x.4solution.no/hour.png" style="height:398px; width:500px" /><br />
+<br />
+<img alt="" src="https://x.4solution.no/month.png" style="height:98px; width:500px" /></p><br />
+</pre>
+	</main>
+    <footer>
+        <p>This stats interface for any Unix/Linux-like system was developed by Arianit Kukaj</p>
+    </footer>
 </body>
 </html>
+
 END
 	return $template;
 }
@@ -35,5 +95,5 @@ $httpd->reg_cb (
       $req->respond ({ content => [ 'text/html', gather() ] });
    }
 );
- 
+
 $httpd->run;
